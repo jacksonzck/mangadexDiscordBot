aad787ab5a5307fda5680e77c7a4d29f67eddbf8
jazck <detterbeckjackson@gmail.com>
2021-05-06 15:27:26 -0500

    Got it sending ping requests

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const https = require('https');
 dotenv.config();
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
+https.get('https://api.mangadex.org/ping', (res) => {
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+        try {
+            //const parsedData = JSON.parse(rawData);
+            console.log(rawData);
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+});
