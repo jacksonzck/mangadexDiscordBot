04fa4e89ab946d30ecf69e274399dfa2eb795465
jazck <detterbeckjackson@gmail.com>
2021-05-06 16:26:14 -0500

    Now if you message pingmebaby it's able to ping the mangadex servers. The wonders of modern technology.

--- main.js
+++ main.js
@@ -4,16 +4,29 @@ const https = require('https');
 dotenv.config();
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
-https.get('https://api.mangadex.org/ping', (res) => {
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-        try {
-            //const parsedData = JSON.parse(rawData);
-            console.log(rawData);
-        } catch (e) {
-            console.error(e.message);
-        }
+
+
+client.on('message', message => {
+    switch (message.content) {
+        case "pingmebaby":
+            ping(message);
+            break;
+    }
+})
+
+function ping(message) {
+    https.get('https://api.mangadex.org/ping', (res) => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                //const parsedData = JSON.parse(rawData);
+                console.log(rawData);
+                message.reply(rawData);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
     });
-});
+}
